12518ed137c1c3ccb37a51caace434a90f50ee1a
kimkonpig <linggooling@gmail.com>
2021-02-19 10:24:54 +0900

    Update M2667.java

--- src/main/java/M2667/M2667.java
+++ src/main/java/M2667/M2667.java
@@ -1,64 +1,83 @@
-package study;
-
 import java.util.*;
 
-/*
-* 진행중
-*/
-
 public class M2667 {
-
-	static int totDanji = 0; //총 단지 수
-	static int[] danji; //단지 내 집의 수;
+	
+	/*
+	 * DFS
+	 * 1. 2중 for문을 통해 값이 1이고, 방문한 적이 없는 시작 정점을 찾는다.
+	 * 2. 찿은 정점을 시작으로 dfs() 재귀 호출해서 아파트 수를 세는 count 변수를 증가시킨다.
+	 * 	* dfs() 재귀 호출 조건
+	 * 		1) 배열의 범위를 넘지 않는다.
+	 * 		2) 상하좌우로 연결되어있다.
+	 * 		3) 방문한 적이 없다.
+	 * 3. 아파트 수를 다 세면 ArrayList인 result에 넣고, count 변수를 0으로 초기화 시킨다.
+	 * 	* result는 각 단지별 아파트 수가 된다.
+	 * 	* result.size()는 아파트 단지 총 개수가 된다.
+	 */
+	
+	static int N; //지도의 크기
+	static int[][] map; //지도 배열화
+	static boolean[][] visit; //방문체크
+	static int count = 0; //아파트 수
+	
+	//result에 각 단지별 아파트 수 저장, result.size()는 아파트 단지 총 개수가 된다.
+	static ArrayList<Integer> result = new ArrayList<Integer>();
+	
+	static int[] dx = {-1, 1, 0, 0}; //상하좌우 체크
+	static int[] dy = {0, 0, -1, 1}; //상하좌우 체크
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int N = sc.nextInt(); //지도의 크기(정사각형, 5<=N<=25)
-		ArrayList<Character>[] al = new ArrayList[N];
-		boolean[] visit = new boolean[N];
+		N = sc.nextInt();
 		
-		danji = new int[N*N];
+		map = new int[N][N]; //지도 초기화
+		visit = new boolean[N][N]; //방문체크 초기화
 		
+		//지도 데이터 셋팅
 		for(int i=0 ; i<N ; i++){
-			al[i] = new ArrayList<Character>();
-		}
-		
-		for(int i=0 ; i<N ; i++){
-			String input = sc.next();
+			String str = sc.next();
 			for(int j=0 ; j<N ; j++){
-				al[i].add(input.charAt(j));
+				map[i][j] = Integer.parseInt(str.substring(j,  j+1));
 			}
-			//System.out.println(al[i]);
 		}
 		
 		for(int i=0 ; i<N ; i++){
 			for(int j=0 ; j<N ; j++){
-				//System.out.println(al[i].get(j));
-				if(al[i].get(j).equals('1') && visit[j]==false){
-					totDanji++;
-					dfs(j, al, visit);
+				if(map[i][j]==1 && !visit[i][j])
+					dfs(i, j);
+				if(count>0){
+					result.add(count);
+					count = 0;
 				}
 			}
 		}
 		
-		Arrays.sort(danji);
-		
-		System.out.println(totDanji);
-		
-		for(int i=0 ; i<danji.length ; i++){
-			if(danji[i] != 0)
-				System.out.println(danji[i]);
+		System.out.println(result.size());//단지 총 개수 출력
+		Collections.sort(result);//오름차순 정렬
+		for(int i : result){
+			System.out.println(i);
 		}
 	}
 	
-	static void dfs(int j, ArrayList<Character>[] al, boolean[] visit){
-		visit[j] = true;
-		danji[totDanji]++;
+	static void dfs(int x, int y){
+		count++; //재귀함수 호출할때마다 단지별 아파트 개수 1 증가
+		visit[x][y] = true; //방문체크
 		
-		for(int k=0 ; k<al.length ; k++){
-			if(visit[k]==false){
-				dfs(k, al, visit);
+		//상하좌우 체크
+		for(int i=0 ; i<4 ; i++){
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			
+			/*
+			 * dfs() 재귀 호출 조건
+	  		 *	1) 배열의 범위를 넘지 않는다.
+	  		 *	2) 상하좌우로 연결되어있다.
+	  		 *	3) 방문한 적이 없다.
+			 */
+			
+			if(nx>=0 && nx<N && ny>=0 && ny<N && map[nx][ny]==1 && !visit[nx][ny]){
+				dfs(nx, ny);
 			}
 		}
 	}
